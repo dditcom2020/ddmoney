3c7849e8f1bbb120989998b208a85c00bf6ecc9e
dditcom2020 <dditcom2020@gmail.com>
2025-09-10 16:25:34 +0700

    ปรับแก้ไข banner

--- app/api/showproduct/route.ts
+++ app/api/showproduct/route.ts
@@ -16,9 +16,11 @@ export async function GET() {
     if (error) {
       return NextResponse.json({ error: error.message }, { status: 500 });
     }
-
     return NextResponse.json(data, { status: 200 });
-  } catch (err: any) {
-    return NextResponse.json({ error: err.message }, { status: 500 });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return NextResponse.json({ error: err.message }, { status: 500 });
+    }
+    return NextResponse.json({ error: "Unexpected error" }, { status: 500 });
   }
 }
